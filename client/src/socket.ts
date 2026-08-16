5e0b959310ef8687d4c58a13b7120de3ffe73aa3
Bharathx7 <bharathvijayakumar7@gmail.com>
2026-08-16 12:02:05 +0530

    chore: configure frontend for production API

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+export const socket = io(
+  import.meta.env.VITE_API_URL || "http://localhost:5000"
+);
 
 socket.on("connect", () => {
   console.log("Connected to Socket.io:", socket.id);
